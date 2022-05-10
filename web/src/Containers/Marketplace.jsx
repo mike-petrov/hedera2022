78d3d56fb8d3b7d6a2c8bf39efe703c57c3d5a69
romanychev-l <leonid20.0831@gmail.com>
2022-05-10 15:43:43 +0300

    upd Marketplace page

--- web/src/Containers/Marketplace.jsx
+++ web/src/Containers/Marketplace.jsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Marketplace = () => {
+const Marketplace = ({ players, address, onExit }) => {
+  const [filter, setFilter] = useState('');
+
+  const onFilter = (e) => {
+		setFilter(e.target.value);
+	};
+
   return (
     <div className="container">
-      <div className="title">Marketplace</div>
-      <div className="subtitle">Build a football team</div>
-      <div className="card_list">
-        1
+      <div className="header">
+        <div className="title">Marketplace</div>
+        <div className="subtitle">Build a football team</div>
+        {address && (
+            <div className="header_block">
+              <span>{address}</span>
+              <FontAwesomeIcon
+                icon={['fas', 'right-from-bracket']}
+                style={{ cursor: 'pointer' }}
+                onClick={onExit}
+              />
+            </div>
+          )}
+      </div>
+      <div className="cards_filter">
+        <div className="card_title">
+          <span>Showing</span>
+          {` ${players && players.filter((player) => player.name.toLowerCase().indexOf(filter.toLocaleLowerCase()) !== -1).length} players`}
+        </div>
+        <input
+          placeholder="Search for players"
+          type="text"
+          value={filter}
+          onChange={onFilter}
+          style={{ width: 'calc(50% - 40px)' }}
+        />
+      </div>
+      <div className="cards_list">
+        <div className="cards_list_inner">
+          {players && players.map((player) => (player.name.toLowerCase().indexOf(filter.toLocaleLowerCase()) !== -1 && (
+            <div className="card" key={player.id}>
+              <img src={player.src} />
+              <div className="card_content">
+                <div className="card_number">{`NO. ${player.id}`}</div>
+                <div className="card_title">
+                  <span>Name</span>
+                  {` ${player.name}`}
+                </div>
+                <div className="card_title">
+                  <span>Position</span>
+                  {` ${player.position}`}
+                </div>
+                <div className="card_title">
+                  <span>Rating</span>
+                  {` ${player.rating}`}
+                </div>
+              </div>
+            </div>
+          )))}
+        </div>
       </div>
     </div>
   );
