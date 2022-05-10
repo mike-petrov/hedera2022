5a0bea35c57ca605a399b354df5211d06de8ec4f
romanychev-l <leonid20.0831@gmail.com>
2022-05-10 15:43:27 +0300

    upd Battles page

--- web/src/Containers/Battles.jsx
+++ web/src/Containers/Battles.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Battles = () => {
+const Battles = ({ address, onExit }) => {
   return (
     <div className="container">
-      <div className="title">Battles</div>
-      <div className="subtitle">Play Daily Against Real Opponents</div>
-      <div className="card_list">
-        1
+      <div className="header">
+        <div className="title">Battles</div>
+        <div className="subtitle">Play Daily Against Real Opponents</div>
+        {address && (
+            <div className="header_block">
+              <span>{address}</span>
+              <FontAwesomeIcon
+                icon={['fas', 'right-from-bracket']}
+                style={{ cursor: 'pointer' }}
+                onClick={onExit}
+              />
+            </div>
+          )}
+      </div>
+      <div className="banner">
+        <img src="./img/star.png" alt="" />
+        <div className="banner_title">Win games, earn and get promoted</div>
+        <div className="banner_subtitle">Buy 5 players to assemble a team</div>
+        <Link
+          to="/marketplace"
+          className="btn"
+          style={{ marginTop: 10 }}
+        >Pick up the best player</Link>
       </div>
     </div>
   );
