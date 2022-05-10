a4c10f11e41746ae4dced7db89ded614ed1c37be
romanychev-l <leonid20.0831@gmail.com>
2022-05-10 15:42:49 +0300

    upd Home page

--- web/src/Containers/Home.jsx
+++ web/src/Containers/Home.jsx
@@ -1,49 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Home = ({ onPopup, myPlayers, address, onConnect }) => {
+const Home = ({ onPopup, myPlayers, address, onConnect, onExit }) => {
+  const [filter, setFilter] = useState('');
+
+  const onFilter = (e) => {
+		setFilter(e.target.value);
+	};
+
   return (
     <div className="container">
-      <div className="title">Academy</div>
-      <div className="subtitle">Your club has its own unique design with colours, kits and a badge</div>
-      <div className="card_list">
-        1
+      <div className="header">
+        <div className="title">Academy</div>
+        <div className="subtitle">Build a football institution</div>
+        {address && (
+            <div className="header_block">
+              <span>{address}</span>
+              <FontAwesomeIcon
+                icon={['fas', 'right-from-bracket']}
+                style={{ cursor: 'pointer' }}
+                onClick={onExit}
+              />
+            </div>
+          )}
       </div>
-      {myPlayers.length > 0 ? (
+      {address ? (
         <>
-          1
+          <div className="cards_filter">
+            <div className="card_title">
+              <span>Showing</span>
+              {` ${myPlayers && myPlayers.filter((player) => player.name.toLowerCase().indexOf(filter.toLocaleLowerCase()) !== -1).length} players`}
+            </div>
+            <input
+              placeholder="Search for players"
+              type="text"
+              value={filter}
+              onChange={onFilter}
+              style={{ width: 'calc(50% - 40px)' }}
+            />
+          </div>
+          <div className="cards_list">
+            <div className="cards_list_inner">
+              {myPlayers && myPlayers.map((player) => (player.name.toLowerCase().indexOf(filter.toLocaleLowerCase()) !== -1 && (
+                <div className="card" key={player.id}>
+                  <img src={player.src} />
+                  <div className="card_content">
+                    <div className="card_number">{`NO. ${player.id}`}</div>
+                    <div className="card_title">
+                      <span>Name</span>
+                      {` ${player.name}`}
+                    </div>
+                    <div className="card_title">
+                      <span>Position</span>
+                      {` ${player.position}`}
+                    </div>
+                    <div className="card_title">
+                      <span>Rating</span>
+                      {` ${player.rating}`}
+                    </div>
+                  </div>
+                </div>
+              )))}
+              {myPlayers && myPlayers.length < 5 && (
+                <div className="card">
+                  <img src="./img/players/0.png" alt=""/>
+                  <div className="card_content">
+                    <div className="card_title">
+                      <span>Buy 5 players to assemble a team </span>
+                    </div>
+                  </div>
+                  <Link
+                    to="/marketplace"
+                    className="btn"
+                    style={{ margin: '0 20px 20px 20px' }}
+                  >Buy players</Link>
+                </div>
+              )}
+            </div>
+          </div>
         </>
       ) : (
-        <div className="stat_block">
-          <div className="stat_block_content">
-            {!address ? (
-              <>
-                <div className="stat_block_icon">
-                  <img src="./img/star.png" alt="" style={{ width: 300, marginBottom: 40 }}/>
-                </div>
-                <div className="stat_block_empty">To get started, connect your wallet</div>
-                <div
-                  className="btn"
-                  style={{ background: '#3e4de5', display: 'block', marginTop: 20 }}
-                  onClick={onConnect}
-                >Connect wallet <FontAwesomeIcon icon={['fas', 'link']} /></div>
-              </>
-            ) : (
-              <>
-                Your Academy
-                <div className="stat_block_icon">
-                  <img src="./img/star.png" alt="" style={{ width: 32, marginBottom: 40 }}/>
-                </div>
-                <div className="stat_block_empty">Time to buy player</div>
-                <Link
-                  to="/map"
-                  className="btn"
-                  style={{ background: '#3e4de5', display: 'block', marginTop: 20 }}
-                >Pick up the best player</Link>
-              </>
-            )}
-          </div>
+        <div className="banner">
+          <img src="./img/star.png" alt="" />
+          <div className="banner_title">Let's play</div>
+          <div className="banner_subtitle">To get started, connect your wallet</div>
+          <div
+            className="btn"
+            style={{ marginTop: 20 }}
+            onClick={onConnect}
+          >Connect wallet <FontAwesomeIcon icon={['fas', 'link']} /></div>
         </div>
       )}
     </div>
