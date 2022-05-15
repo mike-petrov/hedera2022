fa5b5b79bf5a0fab21479317f9591276ddf14c02
mike-petrov <mis5116@yandex.ru>
2022-05-15 18:13:30 +0300

    add Marketplace page

--- web/src/Containers/Marketplace.jsx
+++ web/src/Containers/Marketplace.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Marketplace = ({ players, address, onExit }) => {
+const Marketplace = ({ players, address, tokens, onExit }) => {
   const [filter, setFilter] = useState('');
 
   const onFilter = (e) => {
 		setFilter(e.target.value);
 	};
 
+  const onBuy = (e) => {
+		// setFilter(e.target.value);
+	};
+
   return (
     <div className="container">
       <div className="header">
         <div className="title">Marketplace</div>
-        <div className="subtitle">Build a football team</div>
+        <div className="subtitle">Limited NFT Collection</div>
         {address && (
             <div className="header_block">
+              <span style={{ background: '#3e4de5', display: 'flex', alignItems: 'center' }}>
+                <img src="./img/ball.png" alt="" />
+                <span>{tokens.balls}</span>
+              </span>
+              <span style={{ background: '#3e4de5', display: 'flex', alignItems: 'center' }}>
+                <img src="./img/goal.png" alt="" />
+                <span>{tokens.goals}</span>
+              </span>
               <span>{address}</span>
               <FontAwesomeIcon
                 icon={['fas', 'right-from-bracket']}
@@ -41,7 +53,7 @@ const Marketplace = ({ players, address, onExit }) => {
         <div className="cards_list_inner">
           {players && players.map((player) => (player.name.toLowerCase().indexOf(filter.toLocaleLowerCase()) !== -1 && (
             <div className="card" key={player.id}>
-              <img src={player.src} />
+              <img src={player.src} alt="" />
               <div className="card_content">
                 <div className="card_number">{`NO. ${player.id}`}</div>
                 <div className="card_title">
@@ -56,6 +68,11 @@ const Marketplace = ({ players, address, onExit }) => {
                   <span>Rating</span>
                   {` ${player.rating}`}
                 </div>
+                <div
+                  className="btn"
+                  style={{ margin: '20px 0 0 0' }}
+                  onClick={onBuy}
+                >Buy</div>
               </div>
             </div>
           )))}
